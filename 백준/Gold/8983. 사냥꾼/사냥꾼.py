8ec0573bc67d9b0d57ee64eea21de11eb08a8dbd
klica123456 <139411426+klica123456@users.noreply.github.com>
2023-09-21 09:40:35 +0900

    [Gold IV] Title: 사냥꾼, Time: 408 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,12 +1,17 @@
 M, N, L = map(int, input().split()) # M 사대의 수, N, 동물의 수, L 사정거리
 mx_axis = list(map(int, input().split())) # 사대 M개의 x좌표 값
+mx_axis.sort()
+animal = [tuple(map(int, input().split())) for _ in range(N)]
+animal.sort()
 ans = 0
-for _ in range(N):
-    a, b = map(int, input().split())
+k = 0
+for a, b in animal:
     if b > L:
         continue
-    for i in mx_axis:
-        if abs(i-a)+b <= L:
+    for i in range(k, len(mx_axis)):
+        if abs(mx_axis[i]-a)+b <= L:
+            if i-1 > 0:
+                k = i-1
             ans += 1
             break
 print(ans)
